92bcb77817d4286743beac8caea70e86c4abe996
Caris Williams <cwill1013@gmail.com>
2024-01-24 09:45:26 -0500

    Completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,43 +1,57 @@
-﻿using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-
-namespace OperatorExercise
+﻿namespace OperatorExercise
 {
     public class Program
     {
-        static void Main(string[] args)
-
+        //1.modifier 2. scope 3.return type  4. method name 5. parameter
+        public static double AreaOfCicle(double radius)
         {
+            return Math.PI * Math.Pow(radius, 2);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the radius");
+            double userInput = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(AreaOfCicle(userInput));
+
+
+
+
+
             int a = 17;
             int b = 4;
 
+            //Other operation;
             int sum = a + b;
-            int subtract = a - b;
+            int difference = a - b;
             int product = a * b;
+
+            //Output for other operation
+
+            Console.WriteLine($"Addition: {a} + {b} is {sum}.");
+            Console.WriteLine($"Subtraction: {a} - {b} is {difference}.");
+            Console.WriteLine($"Multiplication: {a} * {b} is {product}.");
+
+
+            //Division integer division
+
             int quotient = a / b;
             int remainder = a % b;
-            Console.WriteLine($"{a} + {b} is {sum}");
-            Console.WriteLine($"{a} - {b} is {subtract}");
-            Console.WriteLine($"{a} + {b} is {product}");
-            Console.WriteLine($"{a} + {b} is {quotient} remainder {remainder}");
+
+            //Output using string interpolation
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
 
 
 
 
-            //1. modifier 2. scope 3. return type 4. method name 5. parameter
 
-            static double AreaOfCircle(double radius)
-            {
-                return Math.PI* Math.Pow(radius, 2);
-            }
-            static void Main(string[] args)
-            {
-                Console.WriteLine("Please enter the radius");
-               double userInput =  double.Parse(Console.ReadLine());
-             
-                Console.WriteLine(AreaOfCircle(userInput));
-             
-            }
-    }
+
+
+
+        }
     }
 }
+
